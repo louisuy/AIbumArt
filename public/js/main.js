13d8abfc0b6355c7084daaf87b5ecde205f4656a
Anthony Bon Louis Uy Cubillas <66798713+louisuy@users.noreply.github.com>
2023-01-07 18:24:55 +0400

    Redirection of the project into a AIbumArt Generator

--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,28 @@
-// require("dotenv").config();
-// const musixmatchApiKey = process.env.MUSIXMATCH_API_KEY;
-// const musixmatchApiKey = 'c5f9e1bcd3ecbcbea1c059aa63851e77';
-
 function onImageSubmit(e) {
-    e.preventDefault();
+    // e.preventDefault();
   
     document.querySelector('.msg').textContent = '';
-    document.querySelector('#image').src = '/placeholder.png';
+    // document.querySelector('#image').src = '';
   
-    const prompt = document.querySelector('#prompt').value;
-    const size = document.querySelector('#size').value;
-    // const size = 'large';
+    var genre = document.querySelector('input[name="genre"]:checked').value;
+    var mood = document.querySelector('input[name="mood"]:checked').value;
+    var time = document.querySelector('input[name="time"]:checked').value;
+
+    const prompt = "generate a DALL-E2 prompt for a " + genre + " " + mood + " " + time + " album, cinematic still, shot on film";
+    const size = 'medium';
   
     if (prompt === '') {
       alert('Please add some text');
-      document.querySelector('#image').src = '/placeholder.png';
+      document.querySelector('#image').src = '../assets/albumart.png';
       return;
     }
-  
+
+    document.getElementById("results").innerHTML = "generate a DALL-E2 prompt for a " + genre + " " + mood + " " + time + " album, cinematic still, shot on film";
     generateImageRequest(prompt, size);
 }
 
-// function onLyricSubmit(e) {
-//     // Prevent the form from submitting and refreshing the page
-//     e.preventDefault();
-
-//     // Get the track name from the form
-//     const trackName = document.getElementById("track-name").value;
-//     console.log(musixmatchApiKey, trackName)
-//     // Call the searchMusixmatch function with the API key
-//     searchMusixmatch(musixmatchApiKey, trackName);
-// }
-
-// function searchMusixmatch(musixmatchApiKey, trackName) {
-//   // Make a request to the Musixmatch API using the fetch API
-//   fetch(`https://api.musixmatch.com/ws/1.1/track.search?apikey=${musixmatchApiKey}&q_track=${trackName}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // Print the API response to the page
-//       const resultDiv = document.getElementById("result");
-//       resultDiv.innerHTML = JSON.stringify(data, null, 2);
-//     });
-// }
-  
 async function generateImageRequest(prompt, size) {
   try {
-    showSpinner();
-
     const response = await fetch('/openai/generateimage', {
       method: 'POST',
       headers: {
@@ -64,25 +40,13 @@ async function generateImageRequest(prompt, size) {
     }
 
     const data = await response.json();
-    // console.log(data);
+    console.log(data);
 
     const imageUrl = data.data;
 
     document.querySelector('#image').src = imageUrl;
-
-    removeSpinner();
+    document.getElementById('bg-image').style.backgroundImage = "url('" + imageUrl + "')";
   } catch (error) {
     document.querySelector('.msg').textContent = error;
   }
 }
-
-function showSpinner() {
-  document.querySelector('.spinner').classList.add('show');
-}
-
-function removeSpinner() {
-  document.querySelector('.spinner').classList.remove('show');
-}
-
-// document.querySelector('#lyric-form').addEventListener('submit', onLyricSubmit);
-document.querySelector('#image-form').addEventListener('submit', onImageSubmit);
